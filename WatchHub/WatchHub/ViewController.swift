7666831436aea9a8b71a6e65ef0fa063b28e5c0e
Rami Mustafa <ramimustafa77@gmail.com>
2025-01-20 23:16:08 +0300

    Project creation.

---
+++ WatchHub/WatchHub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WatchHub
+//
+//  Created by Rami Mustafa on 20.01.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
