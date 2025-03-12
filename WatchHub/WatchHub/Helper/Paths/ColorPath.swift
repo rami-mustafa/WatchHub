1b8d912726c0edcf58428531fb9d2c5302429eab
Rami Mustafa <ramimustafa77@gmail.com>
2025-03-12 23:49:09 +0300

    ColorPath

---
+++ WatchHub/WatchHub/Helper/Paths/ColorPath.swift
@@ -0,0 +1,12 @@
+//
+//  ColorPath.swift
+//  WatchHub
+//
+//  Created by Rami Mustafa on 01.02.25.
+//
+
+import UIKit
+
+class ColorPath {
+    static let mainColor: UIColor = UIColor(named: "mainColor") ?? UIColor()
+}
